029217ca4f18935adb83ac339f7de5bea51b4e62
neosam <neosam+github@posteo.de>
2018-03-14 17:53:46 +0100

    Automatically redirect index to taskapp

--- djangotest/urls.py
+++ djangotest/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
+
+def index(request):
+	return redirect('./taskapp/')
 
 urlpatterns = [
+	path('', index, name="index"),
 	path('taskapp/', include('taskapp.urls')),
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view())
